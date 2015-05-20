f804180a08e3310c6d0c8dafbe5f75e4f76ad5b8
igor <igor.kramaric@gmail.com>
2015-05-20 22:38:16 +0200

    no dpm in here

--- resigner/models.py
+++ resigner/models.py
@@ -1,7 +1,6 @@
-from dpm_core.models import BaseModel
 from django.db import models
 
-class ApiKey(BaseModel):
+class ApiKey(models.Model):
     key = models.CharField(max_length=32)
     secret = models.CharField(max_length=256)
     name = models.CharField(max_length=200)

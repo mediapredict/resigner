7f3385605220e776f7e712fc3f851ba238492568
igor <igor.kramaric@gmail.com>
2015-05-22 14:07:45 +0200

    read.me + c/p fix

--- README.md
+++ README.md
@@ -2,11 +2,58 @@
 
 ## How to install
 
- 1. Put the "resigner" directory somewhere in your python path
+```
+pip install git+git://github.com/mediapredict/resigner.git
+```
 
- 2. Add "resigner" to your installed apps (in the settings.py file)
+## Configuration
+
+In settings.py:
+
+```python
+
+RESIGNER_X_API_KEY = "some_x_api_key"
+
+INSTALLED_APPS = (
+ ...
+    'resigner',
+ ...
+)
+```
+
+Optional
+
+```python
+RESIGNER_API_MAX_DELAY = 1 # max delay in seconds
+```
+
+## Usage (in progress)
+
+Server
+------
+```python
+from django.http import JsonResponse
+
+from resigner.server import signed_req_required
+
+@signed_req_required("MY_API_KEY")
+def my_api_view(request):
+    resp = {"result": "this API has been protected with secret key"}
+    return JsonResponse(resp)
+```
+
+Add MY_API_KEY (key) and my_secret_key (secret) through admin/ApiKeys.
 
 
-## How to use resigner
+Client
+------
 
-Some description
+```python
+...
+res = post_signed(
+    "http://mysite/api_url", {"key": "val"}, settings.RESIGNER_X_API_KEY, "my_secret_key"
+)
+...
+```
+
+Make sure _MY_API_KEY_ has been added as explained above.
--- test_project/resigner_tests/views.py
+++ test_project/resigner_tests/views.py
@@ -1,59 +1,14 @@
-import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-from django.test import LiveServerTestCase
-from django.core.urlresolvers import reverse
-from django.test.client import Client
-from django.conf import settings
+from resigner.server import signed_req_required
 
-from resigner.models import ApiKey
-from resigner.client import post_signed
+@signed_req_required("MY_API_KEY")
+@csrf_exempt
+def my_test_api_view(request):
+    req = request.POST if request.method == 'POST' else request.GET
+    test_data = req.get("MY_TEST_DATA", "")
 
-class TestSignedApi(LiveServerTestCase):
-    def setUp(self):
-        self.client = Client()
-        self.api_key_secret_value = "some_test_secret_value"
+    result = "test ok" if test_data else "no data received"
 
-        ApiKey.objects.create(
-            key="MY_API_KEY",
-            secret=self.api_key_secret_value,
-        )
-
-    def assertApiResult(self, res, result):
-        self.assertEqual(json.loads(res.content)["result"], result)
-
-
-    def call_api(self, data=None,
-                 x_api_key=settings.RESIGNER_X_API_KEY,
-                 api_key_secret_value=None):
-
-        url = self.live_server_url + reverse("my_test_api")
-
-        if data == None:
-            data = {u"MY_TEST_DATA": u"hello from test script!"}
-
-        if api_key_secret_value == None:
-            api_key_secret_value = self.api_key_secret_value
-
-        return post_signed(
-            url, data, x_api_key, api_key_secret_value
-        )
-
-    def test_api_result_ok(self):
-        res = self.call_api()
-
-        self.assertEqual(res.status_code, 200)
-        self.assertApiResult(res, "test ok")
-
-    def test_api_result_nok(self):
-        res = self.call_api({u"SOMETHING_UNEXPECTED": u"some_val"})
-
-        self.assertEqual(res.status_code, 200)
-        self.assertApiResult(res, "no data received")
-
-    def test_wrong_x_api_key(self):
-        res = self.call_api(x_api_key="some_wrong_x_api_key")
-        self.assertEqual(res.status_code, 404)
-
-    def test_wrong_secret(self):
-        res = self.call_api(api_key_secret_value="wrong_secret")
-        self.assertEqual(res.status_code, 404)
+    return JsonResponse({"result":result})

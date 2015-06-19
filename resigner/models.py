1477f427e61db135efe5013437bed0f1a21f3fba
James Robert <jiaaro@gmail.com>
2015-06-18 20:31:29 -0400

    by default auto-generate keys and secrets

--- resigner/models.py
+++ resigner/models.py
@@ -1,8 +1,21 @@
 from django.db import models
+import random
+import string
+
+
+def random_string(length):
+    return ''.join(random.choice(_chars_for_secret) for _ in range(length))
+    
+def mk_key():
+    return random_string(24)
+    
+def mk_secret():
+    return random_string(256)
+
 
 class ApiKey(models.Model):
-    key = models.CharField(max_length=32)
-    secret = models.CharField(max_length=256)
+    key = models.CharField(max_length=32, default=mk_key)
+    secret = models.CharField(max_length=256, default=mk_secret)
     name = models.CharField(max_length=200)
 
     def __unicode__(self):
@@ -10,7 +23,7 @@ class ApiKey(models.Model):
 
 class ApiClient(models.Model):
     name = models.CharField(max_length=200, unique=True)
-    key = models.CharField(max_length=32, unique=True)
+    key = models.CharField(max_length=32, unique=True, default=mk_key)
 
     def __unicode__(self):
         return "%s [%s]" % (self.name, self.id)

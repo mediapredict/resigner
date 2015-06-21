0078239fe41c05abe52a24dfd5fc5a73acf13759
igor <igor.kramaric@gmail.com>
2015-06-21 16:20:57 +0200

    migration (no more ApiClient table)

---
+++ resigner/migrations/0004_auto_20150621_0920.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import resigner.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('resigner', '0003_auto_20150616_0426'),
+    ]
+
+    operations = [
+        migrations.DeleteModel(
+            name='ApiClient',
+        ),
+        migrations.AlterField(
+            model_name='apikey',
+            name='key',
+            field=models.CharField(default=resigner.models.mk_key, max_length=32),
+        ),
+        migrations.AlterField(
+            model_name='apikey',
+            name='secret',
+            field=models.CharField(default=resigner.models.mk_secret, max_length=256),
+        ),
+    ]

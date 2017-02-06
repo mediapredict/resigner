77c146d3111796458fb36538e64984b0b7957ee5
igor <igor.kramaric@gmail.com>
2017-02-06 11:50:38 +0100

    not used

--- test_project/resigner_tests/test_querystring.py
+++ test_project/resigner_tests/test_querystring.py
@@ -2,8 +2,7 @@ import time
 import urlparse
 from urllib import urlencode
 
-from django.core.urlresolvers import reverse
-from django.test import TestCase, Client
+from django.test import TestCase
 
 from resigner.models import ApiKey
 from resigner import querystring

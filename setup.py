c3006e3320d315e6247b7d089369ce2e42851c0a
igor <igor.kramaric@gmail.com>
2017-02-06 17:31:31 +0100

    bump new version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='resigner',
-    version='0.2.1',
+    version='0.3.0',
     author='Mediapredict',
     author_email='engineering@mediapredict.com',
     description=('Django app that provides a way to simply secure your APIs'),

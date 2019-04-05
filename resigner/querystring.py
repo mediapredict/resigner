273afe4292b597105d7f68c0d7e4b05e9ccbfd5d
igor <igor.kramaric@gmail.com>
2019-04-05 09:13:43 +0200

    add expiry param

--- resigner/querystring.py
+++ resigner/querystring.py
@@ -14,30 +14,36 @@ from resigner.models import ApiKey
 TIMESTAMP_TAG = "_timestamp"
 KEY_TAG = "key"
 SIGNATURE_TAG = "signature"
+EXPIRY_TAG = "_expiry"
 
 
 class ValidationError(Exception):
     pass
 
 
-def _generate_signature(params, secret, timestamp):
+def _generate_signature(params, secret, timestamp, expiry):
     signer = Signer(key=secret)
     encoded_params = json.dumps(params, sort_keys=True)
-    return signer.signature(":".join([timestamp, encoded_params]))
+
+    timestamp = str(timestamp)
+    expiry = str(expiry)
+
+    return signer.signature(":".join([timestamp, expiry, encoded_params]))
 
 
-def sign(params, key, secret):
+def sign(params, key, secret, expiry=60*60):
     params = {str(k): str(v) for (k, v) in params.items()}
-    timestamp = str(int(time.time()))
+    timestamp = int(time.time())
 
-    params[SIGNATURE_TAG] = _generate_signature(params, secret, timestamp)
+    params[SIGNATURE_TAG] = _generate_signature(params, secret, timestamp, expiry)
     params[KEY_TAG] = key
     params[TIMESTAMP_TAG] = timestamp
+    params[EXPIRY_TAG] = expiry
 
     return "{}".format(urlencode(params))
 
 
-def validate(querystring, max_age=60*60):
+def validate(querystring):
     params = dict(parse_qsl(querystring))
 
     for key in [TIMESTAMP_TAG, KEY_TAG, SIGNATURE_TAG, ]:
@@ -47,10 +53,13 @@ def validate(querystring, max_age=60*60):
     key = params.pop(KEY_TAG)
     signature = params.pop(SIGNATURE_TAG)
 
-    timestamp = params.pop(TIMESTAMP_TAG)
-    time_stamp_expired = int(timestamp) + max_age
+    try:
+        timestamp = int(params.pop(TIMESTAMP_TAG))
+        expiry = int(params.pop(EXPIRY_TAG))
+    except ValueError:
+        raise ValidationError("Incorrect parameters")
 
-
+    time_stamp_expired = int(timestamp) + int(expiry)
     if time.time() > time_stamp_expired:
         raise ValidationError("Timestamp Expired")
 
@@ -59,8 +68,7 @@ def validate(querystring, max_age=60*60):
     except ApiKey.DoesNotExist:
         raise ValidationError("Key does not exist")
 
-    new_signature = _generate_signature(params, api_key.secret, timestamp)
-
+    new_signature = _generate_signature(params, api_key.secret, timestamp, expiry)
     if new_signature != signature:
         raise ValidationError("Your signature was invalid")
 
--- test_project/resigner_tests/test_querystring.py
+++ test_project/resigner_tests/test_querystring.py
@@ -37,9 +37,13 @@ class TestQueryString(TestCase):
         self.assertTrue(querystring.validate(str(signed_qs)))
 
     def test_fail_if_signed_request_expired(self):
-        signed_qs = querystring.sign({"kfid": 1}, key=self.apikey.key, secret=self.apikey.secret)
-        time.sleep(1)
-        self.assertRaises(querystring.ValidationError, querystring.validate, signed_qs, max_age=0)
+        signed_qs = querystring.sign(
+            {"kfid": 1},
+            key=self.apikey.key,
+            secret=self.apikey.secret,
+            expiry=0
+        )
+        self.assertRaises(querystring.ValidationError, querystring.validate, signed_qs)
 
     def test_fail_if_signed_request_has_wrong_key(self):
         signed_qs = querystring.sign({"kfid": 1}, key="cheese", secret=self.apikey.secret)

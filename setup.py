ba63d9895c499431ceeda2fadc410335e83a6ae1
Chris Curvey <ccurvey@transparint.com>
2021-09-23 08:17:14 -0400

    bumped version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='resigner',
-    version='0.3.5',
+    version='0.3.6',
     author='Mediapredict',
     author_email='engineering@mediapredict.com',
     description=('Django app that provides a way to simply secure your APIs'),
